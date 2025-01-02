44090541f60cb085367f04e6caf62241a6458cc1
Abha Meshram <abha25meshram@gmail.com>
2025-01-02 12:39:51 +0530

    added backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 require("dotenv").config();
 
-const connection=mongoose.connect(process.env.MONGODB_URL);
+const connection=mongoose.connect(process.env.MONGODB_URL,{ 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    connectTimeoutMS: 20000 
+});
 
 module.exports={connection}
