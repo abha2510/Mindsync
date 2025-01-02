fd93f09acee80fd159692f0f5985c9797d48bd1b
Abha Meshram <abha25meshram@gmail.com>
2025-01-02 12:26:12 +0530

    added backend

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const { adminRouter } = require('./Routes/AdminRoute');
 const app = express();
 app.use(express.json());
 const corsOptions = {
-    origin: ['http://localhost:3000','*'],
+    origin: ['http://localhost:3000', 'http://localhost:3001','*'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization, X-Requested-With'
